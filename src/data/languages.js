e9edf32c906d5b0f541de32fed0ad6d49a509568
LouisAntoninLesieur <louisantonin.lesieur@icloud.com>
2025-01-05 23:19:19 +0100

    feat: define available languages

---
+++ src/data/languages.js
@@ -0,0 +1,11 @@
+export const languages = [
+  { name: 'English', value: 'EN-GB' },
+  { name: 'French', value: 'FR' },
+  { name: 'German', value: 'DE' },
+  { name: 'Italian', value: 'IT' },
+  { name: 'Japanese', value: 'JA' },
+  { name: 'Korean', value: 'KO' },
+  { name: 'Portuguese', value: 'PT-PT' },
+  { name: 'Russian', value: 'RU' },
+  { name: 'Chinese', value: 'ZH' },
+];
